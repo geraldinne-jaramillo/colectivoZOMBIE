900017f8402143c4df359031bfd5a5d416adc006
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-01 06:55:47 -0500

    feat Se crean las variables principales

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("******* RESIDENT EVIL BY GLOBERS *******");
+
+        //PERSONAJE: Leon S Kennedy
+        //Nombre
+        //Edad
+        //CantidadVida
+
+        //PERSONAJE: Mr x (TITAN)
+        //nombre
+        //Virus que lo infecto
+        //CantidadVida
+
+        String Personaje1 = "Leon S Kennedy";
+        byte edad;
+        int cantidadVida;
+
+        String Personaje2 = "Mr X";
+        String virus;
+
+        System.out.println("Ingrese la edad de "+Personaje1);
+        
 
 
 
