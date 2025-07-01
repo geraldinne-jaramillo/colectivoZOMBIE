b6cddb97551d5ccdd5552983a21594b48eac08c2
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-01 07:08:01 -0500

    fix ponemos las variables en constantes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,35 @@
-package org.example;
+    package org.example;
 
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+    public class Main {
+        public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("******* RESIDENT EVIL BY GLOBERS *******");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("******* RESIDENT EVIL BY GLOBERS *******");
 
-        //PERSONAJE: Leon S Kennedy
-        //Nombre
-        //Edad
-        //CantidadVida
+            //PERSONAJE: Leon S Kennedy
+            //Nombre
+            //Edad
+            //CantidadVida
 
-        //PERSONAJE: Mr x (TITAN)
-        //nombre
-        //Virus que lo infecto
-        //CantidadVida
+            //PERSONAJE: Mr x (TITAN)
+            //nombre
+            //Virus que lo infecto
+            //CantidadVida
 
-        String Personaje1 = "Leon S Kennedy";
-        byte edad;
-        int cantidadVida;
+            String Personaje1 = "Leon S Kennedy";
+            final byte edad;
+            final int cantidadVida;
 
-        String Personaje2 = "Mr X";
-        String virus;
+            String Personaje2 = "Mr X";
+            final String virus;
 
-        System.out.println("Ingrese la edad de "+Personaje1);
-        
-
+            System.out.println("Ingrese la edad de "+Personaje1);
 
 
+
+
+        }
     }
-}
